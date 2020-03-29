8e393bef4e98532ae9134fc79dc81e06812ec06f
cuttlefishgames <gamescuttlefish@gmail.com>
2020-03-29 01:49:19 -0300

    Updates

--- precache-manifest.b9bddd6d6f49318c46c2704695f0021f.js
+++ precache-manifest.b9bddd6d6f49318c46c2704695f0021f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc7d803c31600bac63c0c545881bbab3",
+    "revision": "8a71d358689fb40cc6fe8140a8eb61c9",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "8ca1d9563dc4d9c50a9d",
+    "revision": "589643382c0b7e84c69d",
     "url": "/portfolio/static/css/main.44bc9dfa.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.99e38603.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8ca1d9563dc4d9c50a9d",
-    "url": "/portfolio/static/js/main.b4efeac8.chunk.js"
+    "revision": "589643382c0b7e84c69d",
+    "url": "/portfolio/static/js/main.4f80221a.chunk.js"
   },
   {
     "revision": "f619aeccc94a0d089131",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9cc3cf4dce1e94ee2f1c30209f97e4d2.js"
+  "/portfolio/precache-manifest.b9bddd6d6f49318c46c2704695f0021f.js"
 );
 
 self.addEventListener('message', (event) => {
