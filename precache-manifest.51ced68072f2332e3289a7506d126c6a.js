5e603ba1cbb3b213ed4047911e6ed03ab01712ae
cuttlefishgames <gamescuttlefish@gmail.com>
2020-03-29 00:32:49 -0300

    Updates

---
+++ precache-manifest.51ced68072f2332e3289a7506d126c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f83505db0bfa5dc2f330df509b93ad0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "53b8c22a457cf4c8ca6e",
+    "url": "/portfolio/static/css/main.44bc9dfa.chunk.css"
+  },
+  {
+    "revision": "71e21c491c9db11642bf",
+    "url": "/portfolio/static/js/2.99e38603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.99e38603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b8c22a457cf4c8ca6e",
+    "url": "/portfolio/static/js/main.b4bc4dc5.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
